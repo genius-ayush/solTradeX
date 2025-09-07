c4d31a386d6d8e0e5750c14fb678cb4fc611829c
genius-ayush <ayushsanjayrawal@gmail.com>
2025-09-07 20:07:31 +0530

    try2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,53 +1,27 @@
-import express, { Request, Response } from 'express' ; 
-
-import dotenv from "dotenv";
 import { Connection, Keypair } from "@solana/web3.js";
 import mongoose from "mongoose";
+// import { bot } from "../../bot/bot"; // adjust path depending on your structure
+import { Request, Response } from "express";
 import { bot } from "./bot/bot";
 
-dotenv.config();
+export const wallets: Record<number, Keypair> = {};
+export const connection = new Connection(process.env.RPC_URL!);
 
-export const wallets: Record<number , Keypair> = {} ; 
-export const connection = new Connection(process.env.RPC_URL!) 
+let isConnected = false;
 
-const app = express(); 
-
-app.use(express.json());
-
-
-
-// import  './bot/handlers/start' ; 
-// import './bot/handlers/help' ; 
-// import './bot/handlers/send' ; 
-// import './bot/handlers/balance' ;
-// import './bot/handlers/fund' ; 
-// import './bot/handlers/wallet' ; 
-// import './bot/handlers/back' ;
-// import './bot/handlers/secretKey' ;
-
-
-
-
-
- async function connectDB() {
+async function connectDB() {
+  if (isConnected) return;
   try {
     await mongoose.connect(process.env.MONGO_SECRET!, { dbName: "bonkbot" });
-    console.log(" Connected to DB");
+    isConnected = true;
+    console.log("Connected to DB");
   } catch (err) {
-    console.error(" DB Connection Error:", err);
+    console.error("DB Connection Error:", err);
   }
 }
 
-// async function bootstrap(){
-//   await connectDB(); 
-//   await bot.launch() ; 
-//   console.log('bot is running') ; 
-// }
-
-// bootstrap() ; 
-
-
-export default async function handler(req : Request, res : Response) {
+// Vercel handler function
+export default async function handler(req:Request, res: Response) {
   await connectDB();
 
   if (req.method === "POST") {
@@ -55,11 +29,10 @@ export default async function handler(req : Request, res : Response) {
       await bot.handleUpdate(req.body, res);
     } catch (err) {
       console.error("Error handling update", err);
-      res.status(500).send("Error");
+      return res.status(500).send("Error");
     }
     return;
   }
 
-  res.status(200).send("BonkBot is live on Vercel 🚀");
+  return res.status(200).send("BonkBot is live on Vercel 🚀");
 }
-
